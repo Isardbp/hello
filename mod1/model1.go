7656e431ab7d5e76e90cc3d560690ba88a99e404
Isardbp <isard.bergonyo@siriondev.com>
2020-01-28 16:03:52 +0100

    t

---
+++ mod1/model1.go
@@ -0,0 +1,6 @@
+package model1
+
+func Hello(param1 string, param2 string, param3 string) string {
+	str := "<a>" + param1 + "</a>" + "<b>" + param2 + "</b>" + "<c>" + param3 + "</c>"
+    return str
+}
